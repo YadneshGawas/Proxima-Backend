faa07403f7cd80383b32427c0b4ff9ff169e736c
YadneshGawas <yadnesh.gawas@gmail.com>
2025-12-17 11:43:13 +0530

    Added gunicorn server

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ echo "Running database migrations..."
 flask db migrate || true
 flask db upgrade
 
-echo "Starting Flask application..."
-python run.py
+echo "Starting production server..."
+gunicorn app.main:app --bind 0.0.0.0:$PORT
